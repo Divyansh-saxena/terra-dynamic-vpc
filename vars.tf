3d6b668739c3b713c5ec5906e15abfcc22e9b35d
Divyansh-saxena <64483506+Divyansh-saxena@users.noreply.github.com>
2021-06-23 10:40:03 +0530

    commit

--- vars.tf
+++ vars.tf
@@ -1,24 +1,24 @@
 
 variable "ENV" {
-  default = "dev"
+  default = "dev" ## leave empty to take input from user
 }
 
 variable "AWS_REGION" {
-  default = "us-east-1"
+  default = "us-east-1" # leave empty to take input from user
 }
 
 variable cidr_block_public{
     type= list
-    default= ["10.0.1.0/24", "10.0.2.0/24"]
+    default= ["10.0.1.0/24", "10.0.2.0/24"] # leave empty to take input from user
 }
 
 variable cidr_block_private{
     type= list
-    default= ["10.0.5.0/24", "10.0.4.0/24"]
+    default= ["10.0.5.0/24", "10.0.4.0/24"] # leave empty to take input from user
 }
 
 variable "az" {
   description = "availability zone"
   type = list
-  default = ["ap-south-1a","ap-south-1b"]
+  default = ["ap-south-1a","ap-south-1b"] # leave empty to take input from user
 } 
